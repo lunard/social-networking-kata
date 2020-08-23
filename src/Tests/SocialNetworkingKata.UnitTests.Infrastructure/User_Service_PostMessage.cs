0fb4487e794cdfd66d85729bd59f09f1e15780a3
Luca Nardelli <lunard@gmail.com>
2020-08-23 16:57:18 +0200

    chore: added asserts to post message test AB#9

--- src/Tests/SocialNetworkingKata.UnitTests.Infrastructure/User_Service_PostMessage.cs
+++ src/Tests/SocialNetworkingKata.UnitTests.Infrastructure/User_Service_PostMessage.cs
@@ -10,18 +10,21 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using User.Service.Application.Commands;
 using User.Service;
+using SocialNetworkingKata.Infrastructure.Data.Interfaces;
 
 namespace SocialNetworkingKata.UnitTests.Services.Infrastructure
 {
     public class User_Service_PostMessage
     {
         UserService userService;
+        IAsyncRepository<UserModel> userRepository;
+
         public User_Service_PostMessage()
         {
             IConfiguration configuration = ConfigurationHelper.BuildConfiguration(AppDomain.CurrentDomain.BaseDirectory);
 
             var uof = new UnitOfWork(configuration);
-            var userRepository = new EfRepository<UserModel>(uof);
+            userRepository = new EfRepository<UserModel>(uof);
 
             userService = new UserService(userRepository);
         }
@@ -29,11 +32,15 @@ namespace SocialNetworkingKata.UnitTests.Services.Infrastructure
         [Fact]
         public async void Post_Test()
         {
-            var command = new CommandPostMessage("Alice", "Hei, this is a new posted message");
+            var message = "Hei, this is a new posted message";
+            var command = new CommandPostMessage("Alice", message);
            
             var commandResult = await userService.Post(command);
 
             Assert.True(commandResult);
+            var aliceUser = (await userRepository.FindAsync(predicate: o => o.Name == "Alice", include: o => o.Include(u => u.Messages))).FirstOrDefault();
+            Assert.NotNull(aliceUser);
+            Assert.Contains(message, aliceUser.Messages.Select(m => m.Content));
         }
     }
 }
